f4412854cddb6f15e5f4ddcdd2527cc61c018799
Leo Fabiano Alves <leo.fabiano@gmail.com>
2020-05-04 15:45:32 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/br/com/leofalves/coursemongo/CoursemongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.leofalves.coursemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemongoApplication.class, args);
+	}
+
+}
