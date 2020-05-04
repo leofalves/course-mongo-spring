e15a26dc7be08dedeac9fe5af4175ed4ec05c0d6
Leo Fabiano Alves <leo.fabiano@gmail.com>
2020-05-04 19:22:04 -0300

    Obtendo usuario por id e tratamento de excecao

--- src/main/java/br/com/leofalves/coursemongo/services/UserService.java
+++ src/main/java/br/com/leofalves/coursemongo/services/UserService.java
@@ -1,12 +1,14 @@
 package br.com.leofalves.coursemongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.leofalves.coursemongo.domain.User;
 import br.com.leofalves.coursemongo.repositories.UserRepository;
+import br.com.leofalves.coursemongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> usr = repo.findById(id);
+		return usr.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
---
+++ src/main/java/br/com/leofalves/coursemongo/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.leofalves.coursemongo.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
