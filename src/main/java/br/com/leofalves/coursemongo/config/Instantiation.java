fa3be010f18214d0d4eca1033f47d6e6b2d87a58
Leo Fabiano Alves <leo.fabiano@gmail.com>
2020-05-04 18:41:50 -0300

    Operação Instanciação da base de dados

---
+++ src/main/java/br/com/leofalves/coursemongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package br.com.leofalves.coursemongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.leofalves.coursemongo.domain.User;
+import br.com.leofalves.coursemongo.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
